292c5bb392d2591819e27eebb9980f24d55b9af2
mapoto40 <84713090+mapoto40@users.noreply.github.com>
2023-06-08 12:22:47 +0200

    Comment added

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import userDB from "./controllers.routes.js";
 const router = Router(); 
 
 router.get('/', (req, res) => res.status(200).send("Server is running"));
+// Path for use api v0.1 
 router.use('/api/v0.1/db', userDB) 
 
 export default router
